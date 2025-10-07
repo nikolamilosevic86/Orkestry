52ac4c9c8de4104cd5e95ece5968e851e7b3ad88
nikola.milosevic <nikola.milosevic86@gmail.com>
2025-10-07 22:45:54 +0200

    Change in docs

--- docs/conf.py
+++ docs/conf.py
@@ -14,8 +14,7 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
-
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.autosummary', 'sphinx.ext.viewcode']
 templates_path = ['_templates']
 exclude_patterns = []
 
